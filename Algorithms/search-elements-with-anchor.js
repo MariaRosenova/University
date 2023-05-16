bbdeff8093a30472781926154e83465e1e8ee533
mims_3 <93831078+MariaRosenova@users.noreply.github.com>
2023-05-16 20:17:40 +0300

    .

---
+++ Algorithms/search-elements-with-anchor.js
@@ -0,0 +1,27 @@
+function anchorSearch(arr, targets) {
+    let i = 0;
+    let count = 0;
+  
+    while (i < arr.length && !targets.includes(arr[i])) {
+      i++;
+      count++;
+    }
+  
+    return { found: i !== arr.length, count };
+  }
+  
+  let myArray = ['а','и', 'ш', 'к', 'о', 'в', 'а'];
+  
+  let searchValues = ['а', 'б', 'в', 'г'];
+  
+  for (let i = 0; i < searchValues.length; i++) {
+    let searchValue = searchValues[i];
+    let result = anchorSearch(myArray, searchValue);
+  
+    if (result.found) {
+      console.log(`Стойността ${searchValue} е намерена в масива след ${result.count} проверки.`);
+    } else {
+      console.log(`Стойността ${searchValue} не е намерена в масива след ${result.count} проверки.`);
+    }
+  }
+  
