fd30a4b89de99e763553490c74ddf60818d7adf9
Maria <mimito1353@gmail.com>
2023-10-12 19:29:42 +0300

    Measure the execution time of the selection sort algorithm for a range from 1000 to 100000.

---
+++ Algorithms/data structures/selection-sort.js
@@ -0,0 +1,41 @@
+function selectionSort(array) {
+  const n = array.length;
+
+  let startTime = performance.now();
+
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < n; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      [array[i], array[minIndex]] = [array[minIndex], array[i]];
+    }
+  }
+}
+
+function measureSelectionSortTime(arraySize) {
+  const randomArray = [];
+  for (let i = 0; i < arraySize; i++) {
+    randomArray.push(Math.random());
+  }
+
+  const startTime = performance.now();
+  selectionSort(randomArray);
+  const endTime = performance.now();
+  let result = endTime - startTime;
+  return result;
+}
+
+const arraySize = [
+  10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000,
+];
+
+for (const size of arraySize) {
+  const executionTime = measureSelectionSortTime(size);
+  console.log(`Array size: ${size}, Execution Time: ${executionTime} ms`);
+}
