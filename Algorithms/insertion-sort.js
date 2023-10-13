1377b50627fc690383ec0a2de8c2fc3d2e2e3e62
Maria <mimito1353@gmail.com>
2023-10-13 11:46:40 +0300

    Measure the execution time of insertion sort algorithm in a range from 1000 to 10 000

--- Algorithms/insertion-sort.js
+++ Algorithms/insertion-sort.js
@@ -1,7 +1,6 @@
 function insertion(arr) {
-  
   let n = arr.length;
-  for (let i = 1; i < n ; i++) {
+  for (let i = 1; i < n; i++) {
     let key = arr[i];
     let j = i - 1;
     while (j >= 0 && arr[j] > key) {
@@ -14,8 +13,24 @@ function insertion(arr) {
   return arr;
 }
 
+function measureInsertionSortTime(arraySize) {
+  const randomArray = [];
+  for (let i = 0; i < arraySize; i++) {
+    randomArray.push(Math.random());
+  }
 
-  const array = [12, 11, 13, 5, 6];
-  const sortedArray =  insertion(array);
-  console.log(sortedArray)
+  const startTime = performance.now();
+  insertion(randomArray);
+  const endTime = performance.now();
 
+  const result = endTime - startTime;
+  return result;
+}
+const arraySize = [
+    10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000,
+  ];
+
+for (const size of arraySize) {
+    const executionTime = measureInsertionSortTime(size);
+    console.log(`Array size: ${size}, Execution Time: ${executionTime} ms`);
+}
