5e456b9b4f8a1ebe89553e765fd3d91d76d410b0
Maria <mimito1353@gmail.com>
2023-10-13 13:33:41 +0300

    Edit the eturn statement

--- Algorithms/bubble-sort.js
+++ Algorithms/bubble-sort.js
@@ -1,26 +1,25 @@
-function bubbleSort(arr){
+function bubbleSort(arr) {
     let counter = 0;
     let swapped = false;
-    do{
-        for(let i = 0; i < arr.length - 1; i++){
-            for(let j = 0; j < (arr.length- 1 - i); j++){
-    
-                /*we check if the item at presented iteration 
-                is greater than the next iteration */
-    
-                if(arr[j] > arr[j + 1]){
-    
-                    //if the condition is true then swap
-                    let result = arr [j];
-                    arr[j] = arr [j + 1];
-                    arr[j + 1] = result;
-                    swapped = true;
-                    counter++;
-                }
-            }
+    do {
+      for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+          /*we check if the item at presented iteration 
+                  is greater than the next iteration */
+  
+          if (arr[j] > arr[j + 1]) {
+            //if the condition is true then swap
+            let result = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = result;
+            swapped = true;
+            counter++;
+          }
         }
-        return `${arr}`;
-    } while(swapped)   
-}
-const array = [234, 43, 55, 63, 5, 6, 235, 547];
-console.log(bubbleSort(array));
+      }
+    } while (swapped);
+    return `${arr}`;
+  }
+  const array = [234, 43, 55, 63, 5, 6, 235, 547];
+  console.log(bubbleSort(array));
+  
